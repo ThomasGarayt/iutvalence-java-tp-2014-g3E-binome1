39ab52aa0d28826f32af0fb523ba78d207645e55
Anthony Gelibert <anthony.gelibert@me.com>
2014-02-21 23:37:06 +0100

    Add TODO (Coordonnees.java)

--- src/Coordonnees.java
+++ src/Coordonnees.java
@@ -1,23 +1,23 @@
-
+/* TODO Javadoc. */
 public class Coordonnees {
 
 	public final int x;
 	public final int y;
 	
-	/* Crée un couple de coordonnées de la forme (x,y)
+	/**
+     * CrÃ©e un couple de coordonnÃ©es de la forme (x,y).
 	 */
-	public Coordonnees(int x1, int y1)
+	public Coordonnees(int x, int y)
 	{
-		this.x = x1;
-		this.y = y1;
+		this.x = x;
+		this.y = y;
 	}
-	
-	public Coordonnees()
+
+    /** TODO CrÃ©er des valeurs random pour x et y (pour le fruit) */
+    public Coordonnees()
 	{
 		x = 0; // TODO 
 		y = 0; // TODO
-	 /* Créer des valeurs random pour x et y (pour le fruit)	
-	  */
 	}
 	
 }
