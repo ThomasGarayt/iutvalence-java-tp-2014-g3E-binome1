029d392aa99e402c821d49b769471d3e44c639fa
Anthony Gelibert <anthony.gelibert@me.com>
2014-02-21 23:38:16 +0100

    Add TODO (MatricePlateau.java)

--- src/MatricePlateau.java
+++ src/MatricePlateau.java
@@ -1,24 +1,19 @@
-
+/* TODO Javadoc. */
 public class MatricePlateau {
+    /* TODO Pourquoi static ? */
+    public static int xPlateau = 20;
+    /* TODO Pourquoi static ? */
+    public static int yPlateau = 20;
 
-	public static int xPlateau = 20;
-	public static int yPlateau = 20;
-	
-	
-	/* Création et initialisation du plateau
-	 */
-	public MatricePlateau()
-	{
-		int matricePlateau[][] = new int[xPlateau][yPlateau];
-		
-		for (int i = 0; i < xPlateau ; i++ )
-		{
-			for (int j = 0; j < yPlateau ; j++ )
-			{
-			matricePlateau[i][j] = 0; 		
-			}
-		}
-	
-		
-	}
+    /** CrÃ©ation et initialisation du plateau */
+    public MatricePlateau() {
+        /* TODO Pourquoi n'est-ce qu'une variable et non un attribut ? */
+        int[][] matricePlateau = new int[xPlateau][yPlateau];
+
+        for (int i = 0; i < xPlateau; i++) {
+            for (int j = 0; j < yPlateau; j++) {
+                matricePlateau[i][j] = 0;
+            }
+        }
+    }
 }
