cffb65d685133660e2c562887e45572a93833d2d
Anthony Gelibert <anthony.gelibert@me.com>
2014-03-18 11:50:49 +0100

    Add TODO (Out.java)

--- src/Out.java
+++ src/Out.java
@@ -1,30 +1,28 @@
-
+/* TODO Javadoc. */
+/* TODO Package. */
+/* TODO Je vous invite à renommer cette classe. */
 public class Out {
-	
-	public Plateau plateau;
-	
-	public Out(Plateau plateau)
-	{
-		this.plateau = plateau;
-	}
-	public void afficherPlateau()
-	{
-		for (int j = 0; j < plateau.getYplateau() ; j++ )
-		{
-			for (int i = 0; i < plateau.getXplateau() ; i++ )
-			{	
-				System.out.print(plateau.getCase(i,j));
-			}
-			System.out.println("");
-		}
-}
 
-	
-	public void nettoyerEcran()
-	{
-		for(int k = 0; k<20 ; k++)
-		{
-			System.out.println("");
-		}
-	}
+    public Plateau plateau;
+
+    public Out(Plateau plateau) {
+        this.plateau = plateau;
+    }
+
+    /* TODO Pourquoi cette classe ici ? */
+    public void afficherPlateau() {
+        for (int j = 0; j < plateau.getYplateau(); j++) {
+            for (int i = 0; i < plateau.getXplateau(); i++) {
+                System.out.print(plateau.getCase(i, j));
+            }
+            System.out.println("");
+        }
+    }
+
+    /* TODO Je vous invite à regarder les codes ANSI pour terminal. */
+    public void nettoyerEcran() {
+        for (int k = 0; k < 20; k++) {
+            System.out.println("");
+        }
+    }
 }
