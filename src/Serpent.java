745a087dc3a59816e1bc53d610017ef9dc6c4663
Anthony Gelibert <anthony.gelibert@me.com>
2014-03-18 11:51:26 +0100

    Add TODO (Serpent.java)

--- src/Serpent.java
+++ src/Serpent.java
@@ -1,34 +1,28 @@
-
+/* TODO Javadoc. */
+/* TODO Package. */
+/* TODO Attention, tout Ã§a dÃ©pend de la taille du tableau. */
 public class Serpent {
+    /* TODO Taille initiale ? */
+    public int tailleSerpent = 4;
 
-	public int tailleSerpent = 4;
-	
-	public SensSerpent sens;
-	
-	public Coordonnees positionSerpent[];
-	
-	public Coordonnees[] getPositionSerpent() {
-		return positionSerpent;
-	}
+    public SensSerpent sens;
 
-	/* Initialisation de la taille et de la position de chaque élément du serpent
-	 * 
-	 */
-	public Serpent()
-	{
-		sens = sens.GAUCHE;
-		positionSerpent = new Coordonnees[] {
-				new Coordonnees(10,10),
-				new Coordonnees(11,10),
-				new Coordonnees(12,10),
-				new Coordonnees(13,10)};
-	}
-	
-	
-	public void avancer()
-	{
-			// Gérer l'avancement du serpent
-			// On modifie le tableau en le décalant à gauche, en faisant Coordonnees[0] + Delta 
-	}
-	
+    public Coordonnees positionSerpent[];
+
+    public Coordonnees[] getPositionSerpent() {
+        return positionSerpent;
+    }
+
+    /* Initialisation de la taille et de la position de chaque Ã©lÃ©ment du serpent.  */
+    public Serpent() {
+        sens = SensSerpent.GAUCHE;
+        positionSerpent = new Coordonnees[]{new Coordonnees(10, 10), new Coordonnees(11, 10), new Coordonnees(12, 10), new Coordonnees(13, 10)};
+    }
+
+    public void avancer() {
+        // Gï¿½rer l'avancement du serpent
+        /* TODO Discutable. */
+        // On modifie le tableau en le dï¿½calant ï¿½ gauche, en faisant Coordonnees[0] + Delta
+    }
+
 }
