4b9700b00b7d61be474c6785986900bf9f828bbe
Anthony Gelibert <anthony.gelibert@me.com>
2014-03-18 11:51:35 +0100

    Add TODO (Snake.java)

--- src/Snake.java
+++ src/Snake.java
@@ -1,35 +1,26 @@
-
+/* TODO Javadoc. */
+/* TODO Package. */
+/* TODO Ã€ discuter : Serpent vs. Snake. */
 public class Snake {
+    private Plateau plateau;
+    private Serpent serpent;
 
-	private Plateau plateau;
-	private Serpent serpent;
-	
-	/*Initialise la matrice du plateau de jeu
-	 *  
-	 */
-	public Snake(int x, int y)
-	{
-		this.plateau = new Plateau(x,y);
-		this.serpent = new Serpent();
-		for (Coordonnees coord : serpent.getPositionSerpent()) {
-			plateau.rajouterSerpent(coord);
-		}
-	}
-	
-	
-	/* Ajoute aléaoirement un fruit de coordonnées (x,y) sur le plateau
-	 * 	
-	 */
-	public void ajouterFruit()
-	{
-		this.plateau.rajouterFruit();
-	}
-	
-	/* Retourne le plateau de jeu
-	 * 
-	 */
-	public Plateau getPlateau()
-	{
-		return this.plateau;
-	}
+    /* Initialise la matrice du plateau de jeu. */
+    public Snake(int x, int y) {
+        this.plateau = new Plateau(x, y);
+        this.serpent = new Serpent();
+        for (Coordonnees coord : serpent.getPositionSerpent()) {
+            plateau.rajouterSerpent(coord);
+        }
+    }
+
+    /* Ajoute alÃ©atoirement un fruit de coordonnÃ©es (x,y) sur le plateau. */
+    public void ajouterFruit() {
+        this.plateau.rajouterFruit();
+    }
+
+    /* Retourne le plateau de jeu. */
+    public Plateau getPlateau() {
+        return this.plateau;
+    }
 }
