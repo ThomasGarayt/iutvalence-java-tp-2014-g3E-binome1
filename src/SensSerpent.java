539d6e969608b48112593a4c69df501a4d304046
Anthony Gelibert <anthony.gelibert@me.com>
2014-03-18 11:51:15 +0100

    Add TODO (SensSerpent.java)

--- src/SensSerpent.java
+++ src/SensSerpent.java
@@ -1,15 +1,16 @@
-
+/* TODO Javadoc. */
+/* TODO Package. */
 public enum SensSerpent {
 	GAUCHE(-1,0),
 	HAUT(0,1),
 	DROITE(1,0),
 	BAS(0,-1);
-	
-	public int getDeltaX() {
-		return TODO;
-	}
-	
+
 	// Faire le constructeur
-	
-	// Gérer les DeltaX et DeltaY
+
+	// GÃ©rer les DeltaX et DeltaY
+    public int getDeltaX() {
+        return 0 /* TODO */;
+    }
+
 }
