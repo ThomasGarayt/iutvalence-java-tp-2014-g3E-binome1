ee99215b9736248ffaadf97aefd5d193d0459f3b
Anthony Gelibert <anthony.gelibert@me.com>
2014-03-18 11:50:02 +0100

    Add TODO (Application.java)

--- src/Application.java
+++ src/Application.java
@@ -1,14 +1,16 @@
+/* TODO Javadoc. */
+/* TODO Package. */
 
 public class Application {
 
-	public static void main(String[] args) {
-		
-		Snake s = new Snake(20,20);
-		Out affichage = new Out(s.getPlateau());
-		
-		affichage.nettoyerEcran();
-		s.ajouterFruit();
-		affichage.afficherPlateau();
-	}
+    public static void main(String[] args) {
+
+        Snake s = new Snake(20, 20);
+        Out affichage = new Out(s.getPlateau());
+
+        affichage.nettoyerEcran();
+        s.ajouterFruit();
+        affichage.afficherPlateau();
+    }
 
 }
